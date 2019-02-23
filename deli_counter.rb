a9b121e301654078f3b6a5d3f718a6fea82b5a56
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 15:22:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(arr)
+new_arr = []
+
+if arr.length == 0
+return "The line is currently empty."
+end
+
+arr.each do |i|
+new_arr.push(" #{(arr.index(i)+1)}. #{i}")
+
+
+end
+return "The line is currently:#{new_arr.join(",")}"
+end 
+
+
